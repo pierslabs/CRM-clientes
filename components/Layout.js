7f17e9ca55e53e3c3f192884f72a08a3d20157fd
pierslabs <pedrolosasp@gmail.com>
2022-01-10 14:34:49 +0100

    login validation

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Head from "next/head";
 import Sidebar from "./Sidebar";
+import { useRouter } from "next/router";
+
 const Layout = ({ children }) => {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -12,14 +15,20 @@ const Layout = ({ children }) => {
           crossOrigin="anonymous"
         />
       </Head>
-      <div className="bg-gray-200 min-h-screen">
-        <div className="flex min-h-screen">
-          <Sidebar />
-          <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
-            {children}
-          </main>
+      {router.pathname === "/login" || router.pathname === "/signup" ? (
+        <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
+          {children}
         </div>
-      </div>
+      ) : (
+        <div className="bg-gray-200 min-h-screen">
+          <div className="flex min-h-screen">
+            <Sidebar />
+            <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+              {children}
+            </main>
+          </div>
+        </div>
+      )}
     </>
   );
 };
--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,33 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const SignUp = () => {
+  // validacion
+  const formik = useFormik({
+    initialValues: {
+      nombre: "",
+      apellido: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      nombre: Yup.string().required("El nombre es obligatorio"),
+      apellido: Yup.string().required("El apellido es obligatorio"),
+      email: Yup.string()
+        .email("El email no es valido")
+        .required("El email es obligatorio"),
+      password: Yup.string()
+        .required("El password es obligatorio")
+        .min(6, "El password debe ser de al menos 6 carácteres "),
+    }),
+    onSubmit: (valores) => {
+      console.log("enviando");
+      console.log(valores);
+    },
+  });
+
   return (
     <Layout>
       <h1 className="text-center text-2xl text-white font-light">
@@ -9,7 +35,10 @@ const SignUp = () => {
       </h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-sm">
-          <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+          <form
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          >
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -23,8 +52,17 @@ const SignUp = () => {
               focus:outline-none focus:shadow-outline"
                 id="nombre"
                 placeholder="Nombre "
+                value={formik.values.nombre}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.nombre && formik.errors.nombre ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.nombre}</p>
+              </div>
+            ) : null}
 
             <div className="mb-4">
               <label
@@ -39,8 +77,17 @@ const SignUp = () => {
               focus:outline-none focus:shadow-outline"
                 id="apellido"
                 placeholder="Apellido "
+                value={formik.values.apellido}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.apellido && formik.errors.apellido ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.apellido}</p>
+              </div>
+            ) : null}
 
             <div className="mb-4">
               <label
@@ -55,8 +102,17 @@ const SignUp = () => {
               focus:outline-none focus:shadow-outline"
                 id="email"
                 placeholder="Email Usuario"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.email && formik.errors.email ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.email}</p>
+              </div>
+            ) : null}
 
             <div className="mb-4">
               <label
@@ -71,8 +127,17 @@ const SignUp = () => {
               focus:outline-none focus:shadow-outline"
                 id="password"
                 placeholder="Password Usuario"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.password && formik.errors.password ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.password}</p>
+              </div>
+            ) : null}
 
             <input
               type="submit"
