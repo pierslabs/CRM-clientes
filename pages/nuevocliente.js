115f34ac138a8a4f643318b64a2346d7de4467a2
pierslabs <pedrolosasp@gmail.com>
2022-01-11 17:45:03 +0100

    vista form registrar clientes

--- pages/nuevocliente.js
+++ pages/nuevocliente.js
@@ -5,7 +5,7 @@ const Nuevocliente = () => {
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
-      <div className="flex jsutify-center mt-5">
+      <div className="flex justify-center mt-5">
         <div className="w-full max-w-lg">
           <form className="bg-white shadow-md px-8 pt-6 pb-8 mb-4">
             <div className="mb-4">
@@ -16,16 +16,97 @@ const Nuevocliente = () => {
                 Nombre
               </label>
               <input
-                type="Nombre"
+                type="text"
                 className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
-              focus:outline-none focus:shadow-outline"
-                id="Nombre"
-                placeholder="Nombre"
+                focus:outline-none focus:shadow-outline"
+                id="nombre"
+                placeholder="Nombre Cliente"
                 // onChange={formik.handleChange}
                 // onBlur={formik.handleBlur}
                 // value={formik.values.email}
               />
             </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="Apellidos"
+              >
+                Apellidos
+              </label>
+              <input
+                type="text"
+                className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+              focus:outline-none focus:shadow-outline"
+                id="apellidos"
+                placeholder="Apellidos Cliente"
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+                // value={formik.values.email}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="Nombre"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+              focus:outline-none focus:shadow-outline"
+                id="email"
+                placeholder="Email Cliente"
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+                // value={formik.values.email}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="empresa"
+              >
+                Empresa
+              </label>
+              <input
+                type="text"
+                className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+              focus:outline-none focus:shadow-outline"
+                id="empresa"
+                placeholder="Empresa"
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+                // value={formik.values.email}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="telefono"
+              >
+                Telefono
+              </label>
+              <input
+                type="tel"
+                className="shadow apperance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight
+              focus:outline-none focus:shadow-outline"
+                id="telefono"
+                placeholder="Telefono Cliente"
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+                // value={formik.values.email}
+              />
+            </div>
+            <input
+              type="submit"
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900"
+              value="Registrar Cliente"
+            />
           </form>
         </div>
       </div>
